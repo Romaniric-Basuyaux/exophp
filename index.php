d4d72a0349004466711e4f62334c4a032d88c637
Romaniric-Basuyaux <romainric.basuyaux@campus-centre.fr>
2025-02-24 15:45:06 +0100

    brief

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form action="form.php" method="post">
+    <label for="name">Nom :</label>
+    <input type="text" id="name" name="name" required>
+    <button type="submit"> Envoyer</button>
+</form>
+
+</body>
+</html>
+<?php
+
+?>
